e3698e335b2fc09cc9ad948443b493b1f88075de
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-02-15 18:02:14 +0000

    Added error handling and improvements using async/await in update-controller.js for the function responsible for editing client information.

--- controllers/update-controller.js
+++ controllers/update-controller.js
@@ -3,6 +3,7 @@ import { clientServices } from "../service/client-service.js";
 // Get reference to the form element in the HTML document
 const form = document.querySelector("[data-form");
 
+/*
 // Function to retrieve and display client information for editing
 const getInfo = () => {
   // Get the current URL
@@ -22,7 +23,7 @@ const getInfo = () => {
   const editEmail = document.querySelector("[data-email]");
   console.log(editName, "---", editEmail);
 
-  /* clientServices.editClient(idUrl).then((profile) => console.log(profile)); */
+  // clientServices.editClient(idUrl).then((profile) => console.log(profile));
 
   // Fetch client information based on the provided ID
   clientServices.editClient(idUrl).then((profile) => {
@@ -30,6 +31,45 @@ const getInfo = () => {
     editName.value = profile.name;
     editEmail.value = profile.email;
   });
+}; */
+
+// Function to retrieve and display client information for editing
+const getInfo = async () => {
+  // Get the current URL
+  const url = new URL(window.location);
+  console.log(url);
+
+  // Extract the client ID from the URL parameters
+  const idUrl = url.searchParams.get("id");
+
+  // Redirect to an error page if no ID is found in the URL
+  if (idUrl === null) {
+    window.location.href = "/screens/error.html";
+  }
+
+  // Get references to HTML elements for editing client information
+  const editName = document.querySelector("[data-name]");
+  const editEmail = document.querySelector("[data-email]");
+  console.log(editName, "---", editEmail);
+
+  // Try to fetch client information based on the provided ID using the editClient service
+  try {
+    // Use async/await to wait for the editClient service to complete and get the profile data
+    const profile = await clientServices.editClient(idUrl);
+    // Check if the retrieved profile data contains both name and email
+    if (profile.name && profile.email) {
+      // If data is complete, set the form fields with the retrieved name and email
+      editName.value = profile.name;
+      editEmail.value = profile.email;
+    } else {
+      // If client information is incomplete, throw an error to trigger the catch block
+      throw new Error();
+    }
+  } catch (error) {
+    // Handle errors by logging the error message and redirecting to the error page
+    console.log("Catch Error - ", error);
+    window.location.href = "/screens/error.html";
+  }
 };
 
 // Invoke the getInfo function when the page loads
